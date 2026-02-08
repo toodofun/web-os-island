9e983d95a8934ad34436f054fd2b0ff8fcc41b44
Rui Ma <535657150@qq.com>
2026-02-08 21:43:57 +0800

    优化dock栏

--- src/components/System/Dock/index.tsx
+++ src/components/System/Dock/index.tsx
@@ -26,7 +26,7 @@ const Dock: React.FC = () => {
     }, [windowManager])
 
     return (
-        <div className="flex items-center justify-between h-full px-4 bg-white/20 backdrop-blur-xs">
+        <div className="flex items-center justify-between gap-3 h-full px-4 bg-white/20 backdrop-blur-xs">
             <div className="flex items-end gap-2">
                 {dockedWindows.map((window) => (
                     <Tooltip key={window.id} content={window.title} showArrow={true}>
@@ -49,28 +49,33 @@ const Dock: React.FC = () => {
                     </Tooltip>
                 ))}
             </div>
-            <div className="flex items-end gap-2">
-                {minimizedWindows.map((window) => (
-                    <Tooltip key={window.id} content={window.title} showArrow={true}>
-                        <div
-                            className="bg-white/30 rounded"
-                            key={window.id}
-                            data-window-target={window.id}
-                            onClick={() => {
-                                windowManager.updateWindow(window.id, {isMinimized: false})
-                                windowManager.bringToFront(window.id)
-                            }}
-                        >
-                            <div
-                                className="cursor-pointer hover:bg-white/50 bg-white/30 h-7 p-0 rounded overflow-hidden"
-                                style={{aspectRatio: 1}}>
-                                <img src={window.icon} alt={window.title}
-                                     className="w-full h-full rounded-md object-contain"/>
-                            </div>
-                        </div>
-                    </Tooltip>
-                ))}
-            </div>
+            {minimizedWindows.length > 0 && (
+                <>
+                    <div className="h-[60%] rounded-xl w-0.75 bg-white/30"></div>
+                    <div className="flex items-end gap-2">
+                        {minimizedWindows.map((window) => (
+                            <Tooltip key={window.id} content={window.title} showArrow={true}>
+                                <div
+                                    className="bg-white/30 rounded"
+                                    key={window.id}
+                                    data-window-target={window.id}
+                                    onClick={() => {
+                                        windowManager.updateWindow(window.id, {isMinimized: false})
+                                        windowManager.bringToFront(window.id)
+                                    }}
+                                >
+                                    <div
+                                        className="cursor-pointer hover:bg-white/50 bg-white/30 h-7 p-0 rounded overflow-hidden"
+                                        style={{aspectRatio: 1}}>
+                                        <img src={window.icon} alt={window.title}
+                                             className="w-full h-full rounded-md object-contain"/>
+                                    </div>
+                                </div>
+                            </Tooltip>
+                        ))}
+                    </div>
+                </>
+            )}
         </div>
     )
 }

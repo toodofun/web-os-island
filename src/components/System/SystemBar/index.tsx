ddb5428c42be72147532b6f27b48cf3fa733922d
Rui Ma <535657150@qq.com>
2026-02-11 20:50:09 +0800

    支持后去当前窗口

--- src/components/System/SystemBar/index.tsx
+++ src/components/System/SystemBar/index.tsx
@@ -2,11 +2,13 @@ import React from "react";
 import DateTime from "@/components/System/Common/DateTime";
 import {Cog6ToothIcon, SparklesIcon, WifiIcon} from "@heroicons/react/24/outline";
 import {Tooltip} from "@heroui/react";
+import {useWindowManager} from "@/components/System/WindowManager";
 
 const SystemBar: React.FC = () => {
+    const windowManager = useWindowManager();
     return (
         <div className="px-4 py-1 flex items-center justify-between">
-            <div>TOS</div>
+            <div>{windowManager.getActiveWindow()?.title || 'TOS'}</div>
             <div className="flex items-center gap-4">
                 {/* 最小化图标栏 */}
                 <div className="flex items-center gap-3">
